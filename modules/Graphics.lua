3de1b88df581fef7b76ac9e3e2f505e58c6b0a4a
Robert Machmer <robert.machmer@gmail.com>
2015-07-28 15:05:58 +0200

    Remove hdrcanvas GraphicsFeature constant

--- modules/Graphics.lua
+++ modules/Graphics.lua
@@ -5257,10 +5257,6 @@ return {
             description = 'Graphics features that can be checked for with love.graphics.isSupported.\n\nsubtractive, mipmap, and dxt are supported on nearly every system.\n\ncanvas, multicanvas, npot, and shader have roughly the same minimum requirements for support: a DirectX 9.0c+ capable graphics card with drivers that support ~OpenGL 2.1+.\n\nhdrcanvas, bc5, and srgb all share mostly the same minimum requirements for support as well: a DirectX 10+ capable graphics card with drivers that support ~OpenGL 3+.\n\nDirectX 9.0c+ capable graphics cards include the nvidia GeForce 5000-series (2003) and newer, the ATI Radeon 9000-series and newer, and the Intel GMA x3100 GPU and newer.\n\nDirectX 10+ capable graphics cards include the nvidia GeForce 8000-series (2006) and newer, the ATI/AMD HD 2000-series and newer, and the Intel HD 2000/3000 GPUs and newer.',
             constants = {
                 {
-                    name = 'hdrcanvas',
-                    description = 'Support for HDR Canvas.'
-                },
-                {
                     name = 'multicanvas',
                     description = 'Support for simultaneous rendering to at least 4 canvases at once, with love.graphics.setCanvas.'
                 },
