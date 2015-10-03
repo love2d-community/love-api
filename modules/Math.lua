a36312a08cac4ef1c7efcdf01492066023e6a840
Robert Machmer <robert.machmer@gmail.com>
2015-10-03 10:36:28 +0200

    Add CompressedData type to love.math

--- modules/Math.lua
+++ modules/Math.lua
@@ -262,6 +262,34 @@ return {
             }
         },
         {
+            name = 'CompressedData',
+            description = 'Represents byte data compressed using a specific algorithm.\n\nlove.math.decompress can be used to de-compress the data.',
+            constructors = {
+                'love.math.compress'
+            },
+            functions = {
+                {
+                    name = 'getFormat',
+                    description = 'Gets the compression format of the CompressedData.',
+                    variants = {
+                        {
+                            returns = {
+                                {
+                                    type = 'CompressedDataFormat',
+                                    name = 'format',
+                                    description = 'The format of the CompressedData.'
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            supertypes = {
+                'Data',
+                'Object'
+            }
+        },
+        {
             name = 'RandomGenerator',
             description = 'A random number generation object which has its own random state.',
             constructors = {
