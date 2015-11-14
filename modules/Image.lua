7a7d51f00e4bbbe7f3f034b03e8bb9f500fb1fb1
Robert Machmer <robert.machmer@gmail.com>
2015-11-14 11:06:05 +0100

    Add new CompressedImageFormat

--- modules/Image.lua
+++ modules/Image.lua
@@ -584,6 +584,54 @@ return {
                 {
                     name = 'bc7srgb',
                     description = 'The sRGB-encoded variant of the BC7 format.',
+                },
+                {
+                    name = 'ETC1',
+                    description = 'The ETC1 format. RGB data at 4 bits per pixel. Suitable for fully opaque images on older Android devices.'
+                },
+                {
+                    name = 'ETC2rgb',
+                    description = 'The RGB variant of the ETC2 format. RGB data at 4 bits per pixel. Suitable for fully opaque images on newer mobile devices.'
+                },
+                {
+                    name = 'ETC2rgba',
+                    description = 'The RGBA variant of the ETC2 format. RGBA data at 8 bits per pixel. Recommended for images with varying opacity on newer mobile devices.'
+                },
+                {
+                    name = 'ETC2rgba1',
+                    description = 'The RGBA variant of the ETC2 format where pixels are either fully transparent or fully opaque. RGBA data at 4 bits per pixel.'
+                },
+                {
+                    name = 'EACr',
+                    description = 'The single-channel variant of the EAC format. Stores just the red channel, at 4 bits per pixel.'
+                },
+                {
+                    name = 'EACrs',
+                    description = 'The signed single-channel variant of the EAC format. Same as above but pixel values in the texture are in the range of [-1, 1] instead of [0, 1] in shaders.'
+                },
+                {
+                    name = 'EACrg',
+                    description = 'The two-channel variant of the EAC format. Stores red and green channels at 8 bits per pixel.'
+                },
+                {
+                    name = 'EACrgs',
+                    description = 'The signed two-channel variant of the EAC format.'
+                },
+                {
+                    name = 'PVR1rgb2',
+                    description = 'The 2 bit per channel RGB variant of the PVRTC1 format. Stores RGB data at 2 bits per pixel. Textures compressed with PVRTC1 formats must be square.'
+                },
+                {
+                    name = 'PVR1rgb4',
+                    description = 'The 4 bit per channel RGB variant of the PVRTC1 format. Stores RGB data at 4 bits per pixel.'
+                },
+                {
+                    name = 'PVR1rgba2',
+                    description = 'The 2 bit per channel RGBA variant of the PVRTC1 format.'
+                },
+                {
+                    name = 'PVR1rgba4',
+                    description = 'The 4 bit per channel RGBA variant of the PVRTC1 format.'
                 }
             }
         },
