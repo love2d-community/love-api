e82a5bc7de2ec66dd69fb43504548781ac7e1a13
Robert Machmer <robert.machmer@gmail.com>
2015-11-24 16:22:28 +0100

    Add Text:addf

--- modules/Graphics.lua
+++ modules/Graphics.lua
@@ -3150,6 +3150,190 @@ return {
                     }
                 },
                 {
+                    name = 'addf',
+                    description = 'Adds additional formatted / colored text to the Text object at the specified position.',
+                    variants = {
+                        {
+                            arguments = {
+                                {
+                                    type = 'string',
+                                    name = 'textstring',
+                                    description = 'The text to add to the object.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'wraplimit',
+                                    description = 'The maximum width in pixels of the text before it gets automatically wrapped to a new line.'
+                                },
+                                {
+                                    type = 'AlignMode',
+                                    name = 'align',
+                                    description = 'The alignment of the text.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'x',
+                                    description = 'The position of the new text on the x-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'y',
+                                    description = 'The position of the new text on the y-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'angle',
+                                    default = '0',
+                                    description = 'The orientation of the object in radians.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'sx',
+                                    default = '1',
+                                    description = 'Scale factor on the x-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'sy',
+                                    default = 'sx',
+                                    description = 'Scale factor on the y-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'ox',
+                                    default = '0',
+                                    description = 'Origin offset on the x-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'oy',
+                                    default = '0',
+                                    description = 'Origin offset on the y-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'kx',
+                                    default = '0',
+                                    description = 'Shearing / skew factor on the x-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'ky',
+                                    default = '0',
+                                    description = 'Shearing / skew factor on the y-axis.'
+                                },
+                            },
+                            returns {
+                                {
+                                    type = 'number',
+                                    name = 'index',
+                                    description = 'An index number that can be used with Text:getWidth or Text:getHeight.'
+                                }
+                            }
+                        },
+                        {
+                            arguments = {
+                                {
+                                    type = 'table',
+                                    name = 'coloredtext',
+                                    description = 'A table containing colors and strings to use as the new text, in the form of { color1, string1, color2, string2, ... }.',
+                                    table = {
+                                        {
+                                            type = 'table',
+                                            name = 'color1',
+                                            description = 'A table containing red, green, blue, and optional alpha components to use as a color for the next string in the table, in the form of {red, green, blue, alpha}.'
+                                        },
+                                        {
+                                            type = 'string',
+                                            name = 'string1',
+                                            description = 'A string of text which has a color specified by the previous color.'
+                                        },
+                                        {
+                                            type = 'table',
+                                            name = 'color2',
+                                            description = 'A table containing red, green, blue, and optional alpha components to use as a color for the next string in the table, in the form of {red, green, blue, alpha}.'
+                                        },
+                                        {
+                                            type = 'string',
+                                            name = 'string2',
+                                            description = 'A string of text which has a color specified by the previous color.'
+                                        }
+                                    }
+                                },
+                                                                {
+                                    type = 'number',
+                                    name = 'wraplimit',
+                                    description = 'The maximum width in pixels of the text before it gets automatically wrapped to a new line.'
+                                },
+                                {
+                                    type = 'AlignMode',
+                                    name = 'align',
+                                    description = 'The alignment of the text.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'x',
+                                    description = 'The position of the new text on the x-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'y',
+                                    description = 'The position of the new text on the y-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'angle',
+                                    default = '0',
+                                    description = 'The orientation of the object in radians.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'sx',
+                                    default = '1',
+                                    description = 'Scale factor on the x-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'sy',
+                                    default = 'sx',
+                                    description = 'Scale factor on the y-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'ox',
+                                    default = '0',
+                                    description = 'Origin offset on the x-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'oy',
+                                    default = '0',
+                                    description = 'Origin offset on the y-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'kx',
+                                    default = '0',
+                                    description = 'Shearing / skew factor on the x-axis.'
+                                },
+                                {
+                                    type = 'number',
+                                    name = 'ky',
+                                    default = '0',
+                                    description = 'Shearing / skew factor on the y-axis.'
+                                },
+                            },
+                            returns {
+                                {
+                                    type = 'number',
+                                    name = 'index',
+                                    description = 'An index number that can be used with Text:getWidth or Text:getHeight.'
+                                }
+                            }
+                        }
+                    }
+                },
+                {
                     name = 'clear',
                     description = 'Clears the contents of the Text object.',
                     variants = {
