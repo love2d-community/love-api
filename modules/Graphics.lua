0107759f3f23632a01338214505d993592895b2f
Robert Machmer <robert.machmer@gmail.com>
2015-12-01 08:53:39 +0100

    Update WrapModes

--- modules/Graphics.lua
+++ modules/Graphics.lua
@@ -6377,11 +6377,11 @@ return {
         },
         {
             name = 'WrapMode',
-            description = 'How the image wraps inside a larger Quad or Mesh.',
+            description = 'How the image wraps inside a Quad with a larger quad size than image size. This also affects how Meshes with texture coordinates which are outside the range of [0, 1] are drawn, and the color returned by the Texel Shader function when using it to sample from texture coordinates outside of the range of [0, 1].',
             constants = {
                 {
                     name = 'clamp',
-                    description = 'Clamp the image. Appears only once.'
+                    description = 'How the image wraps inside a Quad with a larger quad size than image size. This also affects how Meshes with texture coordinates which are outside the range of [0, 1] are drawn, and the color returned by the Texel Shader function when using it to sample from texture coordinates outside of the range of [0, 1].'
                 },
                 {
                     name = 'repeat',
@@ -6389,7 +6389,11 @@ return {
                 },
                 {
                     name = 'mirroredrepeat',
-                    description = 'Repeat the image, flipping it each time it repeats. May produce better visual results than the repeat mode when the image doesn\'t seamlessly tile.'
+                    description = 'Repeat the texture, flipping it each time it repeats. May produce better visual results than the repeat mode when the texture doesn\'t seamlessly tile.'
+                },
+                {
+                    name = 'clampzero',
+                    description = 'Clamp the texture. Fills the area outside the texture\'s normal range with transparent black (or opaque black for textures with no alpha channel.)'
                 }
             }
         }
