e084ae8834bed69486db09cf64af2ca58d8f9fb5
Robert Machmer <robert.machmer@gmail.com>
2015-12-14 10:50:45 +0100

    Update love.video description

--- modules/video/Video.lua
+++ modules/video/Video.lua
@@ -1,6 +1,6 @@
 return {
     name = 'video',
-    description = 'This module is responsible for decoding and streaming video files.\n\nIt can\'t draw the videos, see love.graphics.newVideo and Video objects for that.',
+    description = 'This module is responsible for decoding, controlling, and streaming video files.\n\nIt can\'t draw the videos, see love.graphics.newVideo and Video objects for that.',
     types = {
         require('modules.video.types.Video')
     },
