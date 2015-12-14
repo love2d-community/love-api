cf27a62778759a01a7bbef8ca340abc0892bacc3
Robert Machmer <robert.machmer@gmail.com>
2015-12-14 10:49:46 +0100

    Add Attribute:isAttributeEnabled

--- modules/graphics/types/Mesh.lua
+++ modules/graphics/types/Mesh.lua
@@ -200,6 +200,28 @@ return {
             }
         },
         {
+            name = 'isAttributeEnabled',
+            description = 'Gets whether a specific vertex attribute in the Mesh is enabled. Vertex data from disabled attributes is not used when drawing the Mesh.',
+            variants = {
+                {
+                    arguments = {
+                        {
+                            type = 'string',
+                            name = 'name',
+                            description = 'The name of the vertex attribute to enable or disable.'
+                        }
+                    },
+                    returns = {
+                        {
+                            type = 'boolean',
+                            name = 'enabled',
+                            description = 'Whether the vertex attribute is used when drawing this Mesh.'
+                        }
+                    }
+                }
+            }
+        },
+        {
             name = 'setAttributeEnabled',
             description = 'Enables or disables a specific vertex attribute in the Mesh. Vertex data from disabled attributes is not used when drawing the Mesh.',
             variants = {
