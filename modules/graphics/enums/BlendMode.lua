35d6fd76be4042dd0360e29cead41b02e4102c29
Robert Machmer <robert.machmer@gmail.com>
2015-12-17 10:09:13 +0100

    Update BlendModes

--- modules/graphics/enums/BlendMode.lua
+++ modules/graphics/enums/BlendMode.lua
@@ -1,30 +1,30 @@
 return {
     name = 'BlendMode',
-    description = 'Different ways you do alpha blending.',
+    description = 'Different ways to do color blending. See BlendAlphaMode and the BlendMode Formulas for additional notes.',
     constants = {
         {
-            name = 'add',
-            description = 'Additive blend mode.'
-        },
-        {
             name = 'alpha',
-            description = 'Alpha blend mode ("normal").'
+            description = 'Alpha blending (normal). The alpha of what\'s drawn determines its opacity.'
         },
         {
             name = 'replace',
-            description = 'Replace blend mode.'
+            description = 'The colors of what\'s drawn completely replace what was on the screen, with no additional blending.'
         },
         {
             name = 'screen',
-            description = 'Screen blend mode.'
+            description = '"Screen" blending.'
+        },
+        {
+            name = 'add',
+            description = 'The pixel colors of what\'s drawn are component-wise added to the pixel colors already on the screen.'
         },
         {
             name = 'subtract',
-            description = 'Subtractive blend mode.'
+            description = 'The pixel colors of what\'s drawn are component-wise subtracted from the pixel colors already on the screen.'
         },
         {
             name = 'multiply',
-            description = 'Multiply blend mode.'
+            description = 'The pixel colors of what\'s drawn are component-wise multiplied with the pixel colors already on the screen (effectively darkening them). The alpha of drawn objects is multiplied with the alpha of the screen rather than determining how much the colors on the screen are affected, even if the "alphamultiply" BlendAlphaMode is used.'
         }
     }
 }
