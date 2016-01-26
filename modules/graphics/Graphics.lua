bd367c20c5b97929051e5436595105bd6b17445a
hahawoo <hahawoo@example.com>
2016-01-26 15:04:57 +1100

    Removed outdated love.graphics.newSpriteBatch variants.

--- modules/graphics/Graphics.lua
+++ modules/graphics/Graphics.lua
@@ -1672,56 +1672,6 @@ return {
                 {
                     arguments = {
                         {
-                            type = 'Image',
-                            name = 'image',
-                            description = 'The Image to use for the sprites.'
-                        },
-                        {
-                            type = 'number',
-                            name = 'maxsprites',
-                            default = '1000',
-                            description = 'The maximum number of sprites that the SpriteBatch can contain at any given time.'
-                        }
-                    },
-                    returns = {
-                        {
-                            type = 'SpriteBatch',
-                            name = 'spriteBatch',
-                            description = 'The new SpriteBatch.'
-                        }
-                    }
-                },
-                {
-                    arguments = {
-                        {
-                            type = 'Image',
-                            name = 'image',
-                            description = 'The Image to use for the sprites.'
-                        },
-                        {
-                            type = 'number',
-                            name = 'maxsprites',
-                            default = '1000',
-                            description = 'The maximum number of sprites that the SpriteBatch can contain at any given time.'
-                        },
-                        {
-                            type = 'SpriteBatchUsage',
-                            name = 'usage',
-                            default = '"dynamic"',
-                            description = 'The expected usage of the SpriteBatch. The specified usage mode affects the SpriteBatch\'s memory usage and performance.'
-                        }
-                    },
-                    returns = {
-                        {
-                            type = 'SpriteBatch',
-                            name = 'spriteBatch',
-                            description = 'The new SpriteBatch.'
-                        }
-                    }
-                },
-                {
-                    arguments = {
-                        {
                             type = 'Texture',
                             name = 'texture',
                             description = 'The Image or Canvas to use for the sprites.'
