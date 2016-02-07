f464ed887259672e6b984546a4c8149453e8e5d5
Robert Machmer <robert.machmer@gmail.com>
2016-02-07 04:35:06 +0100

    Add MotorJoint:getAngularOffset and setAngularOffset

--- modules/physics/types/MotorJoint.lua
+++ modules/physics/types/MotorJoint.lua
@@ -6,6 +6,21 @@ return {
     },
     functions = {
         {
+            name = 'getAngularOffset',
+            description = 'Gets the target angluar offset between the two Bodies the Joint is attached to.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'angularoffset',
+                            description = 'The target angular offset in radians: the second body\'s angle minus the first body\'s angle.'
+                        }
+                    }
+                }
+            }
+        },
+        {
             name = 'getLinearOffset',
             description = 'Gets the target linear offset between the two Bodies the Joint is attached to.',
             variants = {
@@ -20,6 +35,21 @@ return {
                             type = 'number',
                             name = 'y',
                             description = 'The y component of the target linear offset, relative to the first Body.'
+                        }
+                    }
+                }
+            }
+        },
+        {
+            name = 'setAngularOffset',
+            description = 'Sets the target angluar offset between the two Bodies the Joint is attached to.',
+            variants = {
+                {
+                    arguments = {
+                        {
+                            type = 'number',
+                            name = 'angularoffset',
+                            description = 'The target angular offset in radians: the second body\'s angle minus the first body\'s angle.'
                         }
                     }
                 }
