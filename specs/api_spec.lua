65c0063eed0fe6e082cf93b481d1848de962eb65
Robert Machmer <robert.machmer@gmail.com>
2016-02-14 15:07:56 +0100

    Add assertion to make sure all values are of type string

--- specs/api_spec.lua
+++ specs/api_spec.lua
@@ -3,4 +3,20 @@ describe( 'LÖVE-API Integrity test', function()
         local api = require( 'love_api' )
         assert( api )
     end)
+
+    it( 'checks that all values are of type string', function()
+        local api = require( 'love_api' )
+
+        local function recursive( index, val )
+            if type( val ) == 'table' then
+                for i, v in pairs( val ) do
+                    recursive( i, v )
+                end
+            elseif type( val ) ~= 'string' then
+                print( index, val )
+                assert.is_True( type( val ) == 'string' )
+            end
+        end
+        recursive( nil, api )
+    end)
 end)
