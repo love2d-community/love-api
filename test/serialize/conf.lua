db8e0a351f4ec90daae2bc2402ee4d92de4ef31e
hahawoo <santos4124@gmail.com>
2017-09-07 01:23:50 +1000

    Add files via upload

---
+++ test/serialize/conf.lua
@@ -0,0 +1,20 @@
+function love.conf(t)
+    t.window = false
+ 
+    t.modules.audio = false
+    t.modules.event = false
+    t.modules.graphics = false
+    t.modules.image = false
+    t.modules.joystick = false
+    t.modules.keyboard = false
+    t.modules.math = false
+    t.modules.mouse = false
+    t.modules.physics = false
+    t.modules.sound = false
+    t.modules.system = false
+    t.modules.timer = false
+    t.modules.touch = false
+    t.modules.video = false
+    t.modules.window = false
+    t.modules.thread = false
+end
