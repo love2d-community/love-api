ae08371aaf56ef2c2ff2e2e39270fa4979bec4c4
Robert Machmer <robert.machmer@gmail.com>
2018-01-10 12:14:36 +0100

    Add Texture:getMipmapCount

--- modules/graphics/types/Texture.lua
+++ modules/graphics/types/Texture.lua
@@ -2,7 +2,23 @@ return {
     name = 'Texture',
     description = 'Superclass for drawable objects which represent a texture. All Textures can be drawn with Quads. This is an abstract type that can\'t be created directly.',
     constructors = {},
-    functions = {},
+    functions = {
+        {
+            name = 'getMipmapCount',
+            description = 'Gets the number of mipmaps contained in the Texture. If the texture was not created with mipmaps, it will return 1.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'mipmaps',
+                            description = 'The number of mipmaps in the Texture.'
+                        }
+                    }
+                }
+            }
+        }
+    },
     parenttype = 'Drawable',
     supertypes = {
         'Drawable',
