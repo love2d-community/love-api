c7403debdf6bf4e93e3ffa2b5a7cff6ce70006ae
Miku AuahDark <auahdark687291@gmail.com>
2020-07-29 22:33:22 +0800

    Add "PixelFormat"
    There are some constants without description.
    
    Furthermore, the description of compressed texture was copied from CompressedImageFormat.

---
+++ modules/image/enums/PixelFormat.lua
@@ -0,0 +1,239 @@
+return {
+    name = 'PixelFormat',
+    description = 'Pixel formats for Textures, ImageData, and CompressedImageData.',
+    constants = {
+        {
+            name = 'unknown',
+        },
+        {
+            name = 'normal',
+            description = 'Alias for rgba8, or srgba8 if gamma-correct rendering is enabled.',
+        },
+        {
+            name = 'hdr',
+        },
+        {
+            name = 'r8',
+        },
+        {
+            name = 'rg8',
+        },
+        {
+            name = 'rgba8',
+        },
+        {
+            name = 'rgba8',
+        },
+        {
+            name = 'srgba8',
+            description = 'gamma-correct version of rgba8.',
+        },
+        {
+            name = 'r16',
+        },
+        {
+            name = 'rg16',
+        },
+        {
+            name = 'rgba16',
+        },
+        {
+            name = 'r16f',
+        },
+        {
+            name = 'rg16f',
+        },
+        {
+            name = 'rgba16f',
+        },
+        {
+            name = 'r32f',
+        },
+        {
+            name = 'rg32f',
+        },
+        {
+            name = 'rgba32f',
+        },
+        {
+            name = 'la8',
+        },
+        {
+            name = 'rgba4',
+        },
+        {
+            name = 'rgb5a1',
+        },
+        {
+            name = 'rgb565',
+        },
+        {
+            name = 'rgb10a2',
+        },
+        {
+            name = 'rg11b10f',
+        },
+        {
+            name = 'stencil8',
+        },
+        {
+            name = 'depth16',
+        },
+        {
+            name = 'depth24',
+        },
+        {
+            name = 'depth32f',
+        },
+        {
+            name = 'depth24stencil8',
+        },
+        {
+            name = 'depth32fstencil8',
+        },
+        {
+            name = 'DXT1',
+            description = 'The DXT1 format. RGB data at 4 bits per pixel (compared to 32 bits for ImageData and regular Images.) Suitable for fully opaque images on desktop systems.',
+        },
+        {
+            name = 'DXT3',
+            description = 'The DXT3 format. RGBA data at 8 bits per pixel. Smooth variations in opacity do not mix well with this format.',
+        },
+        {
+            name = 'DXT5',
+            description = 'The DXT5 format. RGBA data at 8 bits per pixel. Recommended for images with varying opacity on desktop systems.',
+        },
+        {
+            name = 'BC4',
+            description = 'The BC4 format (also known as 3Dc+ or ATI1.) Stores just the red channel, at 4 bits per pixel.',
+        },
+        {
+            name = 'BC4s',
+            description = 'The signed variant of the BC4 format. Same as above but pixel values in the texture are in the range of 1 instead of 1 in shaders.',
+        },
+        {
+            name = 'BC5',
+            description = 'The BC5 format (also known as 3Dc or ATI2.) Stores red and green channels at 8 bits per pixel.',
+        },
+        {
+            name = 'BC5s',
+            description = 'The signed variant of the BC5 format.',
+        },
+        {
+            name = 'BC6h',
+            description = 'The BC6H format. Stores half-precision floating-point RGB data in the range of 65504 at 8 bits per pixel. Suitable for HDR images on desktop systems.',
+        },
+        {
+            name = 'BC6hs',
+            description = 'The signed variant of the BC6H format. Stores RGB data in the range of +65504.',
+        },
+        {
+            name = 'BC7',
+            description = 'The BC7 format (also known as BPTC.) Stores RGB or RGBA data at 8 bits per pixel.',
+        },
+        {
+            name = 'ETC1',
+            description = 'The ETC1 format. RGB data at 4 bits per pixel. Suitable for fully opaque images on older Android devices.',
+        },
+        {
+            name = 'ETC2rgb',
+            description = 'The RGB variant of the ETC2 format. RGB data at 4 bits per pixel. Suitable for fully opaque images on newer mobile devices.',
+        },
+        {
+            name = 'ETC2rgba',
+            description = 'The RGBA variant of the ETC2 format. RGBA data at 8 bits per pixel. Recommended for images with varying opacity on newer mobile devices.',
+        },
+        {
+            name = 'ETC2rgba1',
+            description = 'The RGBA variant of the ETC2 format where pixels are either fully transparent or fully opaque. RGBA data at 4 bits per pixel.',
+        },
+        {
+            name = 'EACr',
+            description = 'The single-channel variant of the EAC format. Stores just the red channel, at 4 bits per pixel.',
+        },
+        {
+            name = 'EACrs',
+            description = 'The signed single-channel variant of the EAC format. Same as above but pixel values in the texture are in the range of 1 instead of 1 in shaders.',
+        },
+        {
+            name = 'EACrg',
+            description = 'The two-channel variant of the EAC format. Stores red and green channels at 8 bits per pixel.',
+        },
+        {
+            name = 'EACrgs',
+            description = 'The signed two-channel variant of the EAC format.',
+        },
+        {
+            name = 'PVR1rgb2',
+            description = 'The 2 bit per pixel RGB variant of the PVRTC1 format. Stores RGB data at 2 bits per pixel. Textures compressed with PVRTC1 formats must be square and power-of-two sized.',
+        },
+        {
+            name = 'PVR1rgb4',
+            description = 'The 4 bit per pixel RGB variant of the PVRTC1 format. Stores RGB data at 4 bits per pixel.',
+        },
+        {
+            name = 'PVR1rgba2',
+            description = 'The 2 bit per pixel RGBA variant of the PVRTC1 format.',
+        },
+        {
+            name = 'PVR1rgba4',
+            description = 'The 4 bit per pixel RGBA variant of the PVRTC1 format.',
+        },
+        {
+            name = 'ASTC4x4',
+            description = 'The 4x4 pixels per block variant of the ASTC format. RGBA data at 8 bits per pixel.',
+        },
+        {
+            name = 'ASTC5x4',
+            description = 'The 5x4 pixels per block variant of the ASTC format. RGBA data at 6.4 bits per pixel.',
+        },
+        {
+            name = 'ASTC5x5',
+            description = 'The 5x5 pixels per block variant of the ASTC format. RGBA data at 5.12 bits per pixel.',
+        },
+        {
+            name = 'ASTC6x5',
+            description = 'The 6x5 pixels per block variant of the ASTC format. RGBA data at 4.27 bits per pixel.',
+        },
+        {
+            name = 'ASTC6x6',
+            description = 'The 6x6 pixels per block variant of the ASTC format. RGBA data at 3.56 bits per pixel.',
+        },
+        {
+            name = 'ASTC8x5',
+            description = 'The 8x5 pixels per block variant of the ASTC format. RGBA data at 3.2 bits per pixel.',
+        },
+        {
+            name = 'ASTC8x6',
+            description = 'The 8x6 pixels per block variant of the ASTC format. RGBA data at 2.67 bits per pixel.',
+        },
+        {
+            name = 'ASTC8x8',
+            description = 'The 8x8 pixels per block variant of the ASTC format. RGBA data at 2 bits per pixel.',
+        },
+        {
+            name = 'ASTC10x5',
+            description = 'The 10x5 pixels per block variant of the ASTC format. RGBA data at 2.56 bits per pixel.',
+        },
+        {
+            name = 'ASTC10x6',
+            description = 'The 10x6 pixels per block variant of the ASTC format. RGBA data at 2.13 bits per pixel.',
+        },
+        {
+            name = 'ASTC10x8',
+            description = 'The 10x8 pixels per block variant of the ASTC format. RGBA data at 1.6 bits per pixel.',
+        },
+        {
+            name = 'ASTC10x10',
+            description = 'The 10x10 pixels per block variant of the ASTC format. RGBA data at 1.28 bits per pixel.',
+        },
+        {
+            name = 'ASTC12x10',
+            description = 'The 12x10 pixels per block variant of the ASTC format. RGBA data at 1.07 bits per pixel.',
+        },
+        {
+            name = 'ASTC12x12',
+            description = 'The 12x12 pixels per block variant of the ASTC format. RGBA data at 0.89 bits per pixel.',
+        },
+    },
+}
