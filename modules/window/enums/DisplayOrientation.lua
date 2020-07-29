344f01b0d46970aad1873cd705ada66c183bec50
Miku AuahDark <auahdark687291@gmail.com>
2020-07-29 21:48:50 +0800

    Oh no.

--- modules/window/enums/DisplayOrientation.lua
+++ modules/window/enums/DisplayOrientation.lua
@@ -1,27 +1,27 @@
 local path = (...):match('(.-)[^%./]+$')
 
 return {
-	name = 'DisplayOrientation',
-	constants = {
-		{
-			name = 'unknown',
-			description = 'Orientation cannot be determined.',
-		},
-		{
-			name = 'landscape',
-			description = 'Landscape orientation.',
-		},
-		{
-			name = 'landscapeflipped',
-			description = 'Landscape orientation (flipped).',
-		},
-		{
-			name = 'portrait',
-			description = 'Portrait orientation.',
-		},
-		{
-			name = 'portraitflipped',
-			description = 'Portrait orientation (flipped).',
-		},
-	},
+    name = 'DisplayOrientation',
+    constants = {
+        {
+            name = 'unknown',
+            description = 'Orientation cannot be determined.',
+        },
+        {
+            name = 'landscape',
+            description = 'Landscape orientation.',
+        },
+        {
+            name = 'landscapeflipped',
+            description = 'Landscape orientation (flipped).',
+        },
+        {
+            name = 'portrait',
+            description = 'Portrait orientation.',
+        },
+        {
+            name = 'portraitflipped',
+            description = 'Portrait orientation (flipped).',
+        },
+    },
 }
