8efd8a4af5c2b97fb9b70b81bc48a1076e9d2557
Miku AuahDark <auahdark687291@gmail.com>
2020-07-31 17:41:01 +0800

    Rasterizer

--- modules/font/types/Rasterizer.lua
+++ modules/font/types/Rasterizer.lua
@@ -1,60 +1,165 @@
 return {
     name = 'Rasterizer',
-    supertypes = {'Object',},
+    description = 'A Rasterizer handles font rendering, containing the font data (image or TrueType font) and drawable glyphs.',
+    supertypes = {
+        'Object',
+    },
     functions = {
         {
             name = 'getAdvance',
+            description = 'Gets font advance.',
             variants = {
                 {
                     returns = {
-                        {type = 'number'}
-                    }
-                }
-            }
+                        {
+                            type = 'number',
+                            name = 'advance',
+                            description = 'Font advance.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getAscent',
+            description = 'Gets ascent height.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'height',
+                            description = 'Ascent height.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getDescent',
+            description = 'Gets descent height.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'height',
+                            description = 'Descent height.',
+                        },
+                    },
+                },
+            },
         },
         {
             name = 'getGlyphCount',
+            description = 'Gets number of glyphs in font.',
             variants = {
                 {
                     returns = {
-                        {type = 'number'}
-                    }
-                }
-            }
+                        {
+                            type = 'number',
+                            name = 'count',
+                            description = 'Glyphs count.',
+                        },
+                    },
+                },
+            },
         },
         {
             name = 'getGlyphData',
+            description = 'Gets glyph data of a specified glyph.',
             variants = {
                 {
                     arguments = {
-                        {type = 'string', name = 'glyph'}
+                        {
+                            type = 'string',
+                            name = 'glyph',
+                            description = 'Glyph',
+                        },
                     },
                     returns = {
-                        {type = 'GlyphData'}
-                    }
+                        {
+                            type = 'GlyphData',
+                            name = 'glyphData',
+                            description = 'Glyph data',
+                        },
+                    },
                 },
                 {
                     arguments = {
-                        {type = 'number', name = 'glyphNumber'}
+                        {
+                            type = 'number',
+                            name = 'glyphNumber',
+                            description = 'Glyph number',
+                        },
                     },
                     returns = {
-                        {type = 'GlyphData'}
-                    }
-                }
-            }
+                        {
+                            type = 'GlyphData',
+                            name = 'glyphData',
+                            description = 'Glyph data',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getHeight',
+            description = 'Gets font height.',
+            variants = {
+                {
+                    returns = {
+                        type = 'number',
+                        name = 'height',
+                        description = 'Font height',
+                    },
+                },
+            },
+        },
+        {
+            name = 'getLineHeight',
+            description = 'Gets line height of a font.',
+            variants = {
+                {
+                    returns = {
+                        type = 'number',
+                        name = 'height',
+                        description = 'Line height of a font.',
+                    },
+                },
+            },
         },
         {
             name = 'hasGlyphs',
+            description = 'Checks if font contains specified glyphs.',
             variants = {
                 {
                     arguments = {
-                        {type = 'string or number', name = '...'}
+                        {
+                            type = 'string or number',
+                            name = 'glyph1',
+                            description = 'Glyph',
+                        },
+                        {
+                            type = 'string or number',
+                            name = 'glyph2',
+                            description = 'Glyph',
+                        },
+                        {
+                            type = 'string or number',
+                            name = '...',
+                            description = 'Additional glyphs',
+                        },
                     },
                     returns = {
-                        {type = 'boolean'}
-                    }
-                }
-            }
-        }
-    }
+                        {
+                            type = 'boolean',
+                            name = 'hasGlyphs',
+                            description = 'Whatever font contains specified glyphs.',
+                        },
+                    },
+                },
+            },
+        },
+    },
 }
