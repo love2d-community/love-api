3d74c43ccad4483f0ccd8c8f7bf6fd520ae0bb5b
Miku AuahDark <auahdark687291@gmail.com>
2020-07-31 18:06:15 +0800

    GlyphData

--- modules/font/types/GlyphData.lua
+++ modules/font/types/GlyphData.lua
@@ -15,7 +15,152 @@ return {
                         {
                             type = 'number',
                             name = 'advance',
-                            description = 'Glyph advance.'
+                            description = 'Glyph advance.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getBearing',
+            description = 'Gets glyph bearing.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'bx',
+                            description = 'Glyph bearing X.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'by',
+                            description = 'Glyph bearing Y.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getBoundingBox',
+            description = 'Gets glyph bounding box.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'x',
+                            description = 'Glyph position x.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'y',
+                            description = 'Glyph position y.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'width',
+                            description = 'Glyph width.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'height',
+                            description = 'Glyph height.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getDimensions',
+            description = 'Gets glyph dimensions.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'width',
+                            description = 'Glyph width.',
+                        },
+                        {
+                            type = 'number',
+                            name = 'height',
+                            description = 'Glyph height.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getFormat',
+            description = 'Gets glyph pixel format.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'PixelFormat',
+                            name = 'format',
+                            description = 'Glyph pixel format.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getGlyph',
+            description = 'Gets glyph number.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'glyph',
+                            description = 'Glyph number.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getGlyphString',
+            description = 'Gets glyph string.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'string',
+                            name = 'glyph',
+                            description = 'Glyph string.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getHeight',
+            description = 'Gets glyph height.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'height',
+                            description = 'Glyph height.',
+                        },
+                    },
+                },
+            },
+        },
+        {
+            name = 'getWidth',
+            description = 'Gets glyph width.',
+            variants = {
+                {
+                    returns = {
+                        {
+                            type = 'number',
+                            name = 'width',
+                            description = 'Glyph width.',
                         },
                     },
                 },
