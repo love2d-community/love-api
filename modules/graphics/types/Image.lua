37157766bf0079307961107c8c5e017db1bcafea
Miku AuahDark <auahdark687291@gmail.com>
2020-07-31 20:12:17 +0800

    Deduplication of Canvas and Image functions.

--- modules/graphics/types/Image.lua
+++ modules/graphics/types/Image.lua
@@ -10,65 +10,6 @@ return {
     },
     functions = {
         {
-            name = 'getDimensions',
-            description = 'Gets the width and height of the Image.',
-            variants = {
-                {
-                    returns = {
-                        {
-                            type = 'number',
-                            name = 'width',
-                            description = 'The width of the Image, in pixels.',
-                        },
-                        {
-                            type = 'number',
-                            name = 'height',
-                            description = 'The height of the Image, in pixels.',
-                        },
-                    },
-                },
-            },
-        },
-        {
-            name = 'getFilter',
-            description = 'Gets the filter mode for an image.',
-            variants = {
-                {
-                    returns = {
-                        {
-                            type = 'FilterMode',
-                            name = 'min',
-                            description = 'Filter mode used when minifying the image.',
-                        },
-                        {
-                            type = 'FilterMode',
-                            name = 'mag',
-                            description = 'Filter mode used when magnifying the image.',
-                        },
-                    },
-                },
-                {
-                    returns = {
-                        {
-                            type = 'FilterMode',
-                            name = 'min',
-                            description = 'Filter mode used when minifying the image.',
-                        },
-                        {
-                            type = 'FilterMode',
-                            name = 'mag',
-                            description = 'Filter mode used when magnifying the image.',
-                        },
-                        {
-                            type = 'number',
-                            name = 'anisotropy',
-                            description = 'Maximum amount of anisotropic filtering used.',
-                        },
-                    },
-                },
-            },
-        },
-        {
             name = 'getFlags',
             description = 'Gets the flags used when the image was created.',
             variants = {
@@ -84,50 +25,15 @@ return {
             },
         },
         {
-            name = 'getHeight',
-            description = 'Gets the height of the Image.',
+            name = 'isCompressed',
+            description = 'Gets whether the Image was created from CompressedData.\n\nCompressed images take up less space in VRAM, and drawing a compressed image will generally be more efficient than drawing one created from raw pixel data.',
             variants = {
                 {
                     returns = {
                         {
-                            type = 'number',
-                            name = 'height',
-                            description = 'The height of the Image, in pixels.',
-                        },
-                    },
-                },
-            },
-        },
-        {
-            name = 'getWidth',
-            description = 'Gets the width of the Image.',
-            variants = {
-                {
-                    returns = {
-                        {
-                            type = 'number',
-                            name = 'width',
-                            description = 'The width of the Image, in pixels.',
-                        },
-                    },
-                },
-            },
-        },
-        {
-            name = 'getWrap',
-            description = 'Gets the wrapping properties of an Image.\n\nThis function returns the currently set horizontal and vertical wrapping modes for the image.',
-            variants = {
-                {
-                    returns = {
-                        {
-                            type = 'WrapMode',
-                            name = 'horiz',
-                            description = 'Horizontal wrapping mode of the image.',
-                        },
-                        {
-                            type = 'WrapMode',
-                            name = 'vert',
-                            description = 'Vertical wrapping mode of the image.',
+                            type = 'boolean',
+                            name = 'compressed',
+                            description = 'Whether the Image is stored as a compressed texture on the GPU.',
                         },
                     },
                 },
@@ -171,66 +77,6 @@ return {
                             type = 'boolean',
                             name = 'reloadmipmaps',
                             description = 'Whether to generate new mipmaps after replacing the Image\'s pixels. True by default if the Image was created with automatically generated mipmaps, false by default otherwise.',
-                        },
-                    },
-                },
-            },
-        },
-        {
-            name = 'setFilter',
-            description = 'Sets the filter mode for an image.',
-            variants = {
-                {
-                    arguments = {
-                        {
-                            type = 'FilterMode',
-                            name = 'min',
-                            description = 'How to scale an image down.',
-                        },
-                        {
-                            type = 'FilterMode',
-                            name = 'mag',
-                            description = 'How to scale an image up.',
-                        },
-                    },
-                },
-                {
-                    arguments = {
-                        {
-                            type = 'FilterMode',
-                            name = 'min',
-                            description = 'How to scale an image down.',
-                        },
-                        {
-                            type = 'FilterMode',
-                            name = 'mag',
-                            description = 'How to scale an image up.',
-                        },
-                        {
-                            type = 'number',
-                            name = 'anisotropy',
-                            description = 'Maximum amount of anisotropic filtering used.',
-                            default = '1',
-                        },
-                    },
-                },
-            },
-        },
-        {
-            name = 'setWrap',
-            description = 'Sets the wrapping properties of an Image.\n\nThis function sets the way an Image is repeated when it is drawn with a  Quad that is larger than the image\'s extent. An image may be clamped or set to repeat in both horizontal and vertical directions. Clamped images appear only once, but repeated ones repeat as many times as there is room in the Quad.\n\nN.B. If you use a Quad that is larger than the image extent and do not use repeated tiling, there may be an unwanted visual effect of the image stretching all the way to fill the Quad. If this is the case, setting Image:setWrap(\'repeat\', \'repeat\') for all the images to be repeated, and using Quads of appropriate size will result in the best visual appearance.',
-            variants = {
-                {
-                    arguments = {
-                        {
-                            type = 'WrapMode',
-                            name = 'horiz',
-                            description = 'Horizontal wrapping mode of the image.',
-                        },
-                        {
-                            type = 'WrapMode',
-                            name = 'vert',
-                            description = 'Vertical wrapping mode of the image.',
                         },
                     },
                 },
